c36ea054c986b8f59685d0a2e576ef582d58cd7c
Alessio <alessiotortora@gmail.com>
2023-04-01 14:00:51 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(" https://restcountries.com/v3.1/all");
+    setData(response.data);
+  };
+
+  const handleFetch = () => {
+    fetchData();
+  };
+
+  console.log(data[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleFetch}>Refresh</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Flag</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((country, index) => (
+            <tr key={index}>
+              <td>{country.name.common}</td>
+              <td>{country.flag}</td>
+              <td>{country.population || 'N/A'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
